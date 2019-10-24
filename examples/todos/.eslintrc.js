377fd864e54941f96e701e21b80aa856c8e40f2d
ralph-cap <ralph-cap@users.noreply.github.com>
2019-10-24 19:14:15 +0200

    eslint

---
+++ examples/todos/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: 'react-app',
+
+  settings: {
+    react: {
+      version: '16.10'
+    }
+  },
+
+  rules: {
+    'jsx-a11y/href-no-hash': 'off',
+    'no-unused-vars': 'off'
+  }
+}
